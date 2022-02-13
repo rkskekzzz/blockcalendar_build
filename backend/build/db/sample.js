f56ab4d4953315ab2342fb7864ee24d163abd3b8
rkskekzzz <wkdlfflxh@naver.com>
2022-02-13 14:13:44 +0000

    Build: 백엔드

--- backend/build/db/sample.js
+++ backend/build/db/sample.js
@@ -2,19 +2,24 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.sampleUser = void 0;
 const entity_1 = require("../interface/entity");
+function getDay(offsetFromToday) {
+    const curr = new Date();
+    const day = new Date(curr.getFullYear(), curr.getMonth() + 1, curr.getDate() + offsetFromToday, 24);
+    return day.toISOString();
+}
 exports.sampleUser = {
     name: "sample",
     color: "#ff0000",
     schedules: [
         {
             valid: entity_1.ScheduleValidType.POSIBLE,
-            start: Date.now().toString(),
-            end: Date.now().toString(),
+            start: getDay(0),
+            end: getDay(0),
         },
         {
             valid: entity_1.ScheduleValidType.IMPOSIBLE,
-            start: (Date.now() + 1).toString(),
-            end: (Date.now() + 1).toString(),
+            start: getDay(1),
+            end: getDay(1),
         },
     ],
 };
